cde65efa0dc81764ed4059d42163775cd800fcd1
Ravindra-krishnan <rankrish13@gmail.com>
2023-01-30 14:07:33 +0530

    Intial commit

---
+++ cryptonote-master/src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x42, 0x51, 0x51, 0x50, 0x58, 0x4a, 0x4e, 0x4f, 0x54, 0x45, 0x01, 0x13, 0x13, 0x10, 0x06, 0x20 } };
+}
